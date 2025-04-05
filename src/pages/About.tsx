cd8cf296354fed91f934d1613a95f7bd9d8f847c
patrickmetz1 <patbmetz@gmail.com>
2025-04-04 22:35:16 -0500

    Update About.tsx
    adjusting the padding on about page again

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
       <Navbar />
       
       {/* Enhanced Mission Section - Added more top padding */}
-      <section className="section-padding pt-52 pb-28">
+      <section className="section-padding pt-54 pb-18">
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto">
             <div className="text-center mb-16">
