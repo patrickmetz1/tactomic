594218635691fe8a60f86faceb99138f46002960
patrickmetz1 <patbmetz@gmail.com>
2025-05-04 16:06:36 -0500

    Update Footer.tsx
    resizing the footer images badges

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,12 +45,12 @@ const Footer = () => {
             <img
               src={IntuitBookkeeper}
               alt="Intuit Trained Bookkeeper"
-              className="h-18 object-contain"
+              className="h-16 object-contain"
             />
             <img
               src={XeroCertified}
               alt="Xero Certified"
-              className="h-22 object-contain"
+              className="h-16 object-contain"
             />
           </div>
         </div>
