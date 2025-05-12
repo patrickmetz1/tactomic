7563aa3dedc144a43be5a81b16963eb51280cef6
patrickmetz1 <patbmetz@gmail.com>
2025-05-12 11:29:08 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -33,10 +33,10 @@ const Index = () => {
     <div className="min-h-screen">
       <Navbar />
       <Hero />
+      <Empathy />
       <Benefits />
       <Services />
       <Pricing />
-      <Empathy />
       <CTA />
       <Footer />
     </div>
