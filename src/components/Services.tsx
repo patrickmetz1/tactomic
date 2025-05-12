4ca2b422a1befd4451f16c4368bef10de069c53d
patrickmetz1 <patbmetz@gmail.com>
2025-05-12 11:31:10 -0500

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from './ui/card';
 const serviceItems = [{
   icon: <Database className="w-6 h-6" />,
   title: "Frictionless Accounting",
-  description: "Get access to real time financial informatoin any time of month, maintained in both cash and accrual basis so you are always up to speed and ready for tax season.",
+  description: "Get access to real time financial informatoin any time of month, maintained in both cash and accrual basis so you are always up to speed.",
   color: "bg-purple-50 text-purple-600"
 }, {
   icon: <Calculator className="w-6 h-6" />,
